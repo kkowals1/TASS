80200a45667a991585ec940c76ddbcd7cb4eb5b4
kkowals1 <59780385+kkowals1@users.noreply.github.com>
2023-01-13 19:24:30 +0100

    Update TASS_app_gui.py

--- TASS_app_gui.py
+++ TASS_app_gui.py
@@ -5,17 +5,43 @@ from tkinter import *
 from tkinter import messagebox
 import graph
 
-list_of_airports = data.create_list_of_airports()
+graphik = graph.create_graph()
+# Stwórz listę objektów linii lotniczych, lotnisk oraz tras
+airlines = data.create_list_of_airlines()
+airports = data.create_list_of_airports()
+routes = data.create_list_of_routes()
 
-#Globalna zmienna - wynik szukania (OPCJA B)
-result = "123"
 
 def list_of_airports_names():
     l = []
-    for i in list_of_airports:
+    for i in airports:
         name = i.name
         l.append(name)
     return l
+
+
+def search_source(event):
+    value = event.widget.get()
+    if value == '':
+        source_combobox['values'] = list_of_airports_names()
+    else:
+        temp_list = []
+        for item in list_of_airports_names():
+            if value.lower() in item.lower():
+                temp_list.append(item)
+        source_combobox['values'] = temp_list
+
+
+def search_destination(event):
+    value = event.widget.get()
+    if value == '':
+        destination_combobox['values'] = list_of_airports_names()
+    else:
+        temp_list = []
+        for item in list_of_airports_names():
+            if value.lower() in item.lower():
+                temp_list.append(item)
+        destination_combobox['values'] = temp_list
 
 
 def find_result():
@@ -26,19 +52,22 @@ def find_result():
         if source == destination:
             tk.messagebox.showwarning(title="Error", message="Source and destination airports cannot be the same!")
         else:
-            source_id = str(data.get_id_by_airport_name(list_of_airports, source))
-            destination_id = str(data.get_id_by_airport_name(list_of_airports, destination))
+            source_id = str(data.get_id_by_airport_name(airports, source))
+            destination_id = str(data.get_id_by_airport_name(airports, destination))
+            print(source_id, destination_id)
+            result = "123"
+            score = graph.find_best_connection(given_graph=graphik, node1=source_id, node2=destination_id, airlines=airlines,
+                                               airports=airports, routes=routes)
+            print(score)
 
-            # OPCJA A
-            #TUTAJ WYWOŁAĆ FUNKCJE I JAZDA - graph.find_best_connection(lista, source_id, destination_id)
-            #result = "123"
-            #answer.insert(INSERT, result)
-            #answer.pack()
+            # answer.insert(INSERT, result)
+            # answer.pack()
+            my_label.config(text=score)
 
-            #OPCJA B (lepsze user experience)
-            my_label.config(text=result)
+
     else:
         tk.messagebox.showwarning(title="Error", message="Source and destination airports are required!")
+
 
 window = tk.Tk()
 window.title("Flying App")
@@ -54,11 +83,13 @@ source_airport_label = tk.Label(flight_params_frame, text="Source airport:")
 source_combobox = ttk.Combobox(flight_params_frame, width=40, values=list_of_airports_names())
 source_airport_label.grid(row=0, column=0)
 source_combobox.grid(row=1, column=0)
+source_combobox.bind('<KeyPress>', search_source)
 
 destination_airport_label = tk.Label(flight_params_frame, text="Destination airport:")
 destination_combobox = ttk.Combobox(flight_params_frame, width=40, values=list_of_airports_names())
 destination_airport_label.grid(row=0, column=1)
 destination_combobox.grid(row=1, column=1)
+destination_combobox.bind('<KeyPress>', search_destination)
 
 confirm_button = tk.Button(flight_params_frame, text="Find my flights", command=find_result)
 confirm_button.grid(row=1, column=2, sticky="news", padx=20, pady=10)
@@ -77,6 +108,6 @@ terms_check.grid(row=0, column=0, padx=10, pady=10)
 # Create a Label widget
 my_label = Label(terms_check, text="")
 my_label.pack()
-#answer = Text(terms_check, width=50, height=1)
+# answer = Text(terms_check, width=50, height=1)
 
 window.mainloop()

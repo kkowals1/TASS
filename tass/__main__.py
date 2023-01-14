118a9908ee0209cc4e672559dfc1baade6bf1d02
Pruxon <jasiekpr@gmail.com>
2023-01-14 17:17:24 +0100

    better gui

--- tass/__main__.py
+++ tass/__main__.py
@@ -7,7 +7,6 @@ import tass.graph as graph
 
 if __name__ == "__main__":
     graphik = graph.create_graph()
-    # Stwórz listę objektów linii lotniczych, lotnisk oraz tras
     routes = data.create_list_of_routes()
     airlines = data.create_list_of_airlines()
     airports = data.create_list_of_airports(routes)
@@ -55,17 +54,11 @@ if __name__ == "__main__":
             else:
                 source_id = str(data.get_id_by_airport_name(airports, source))
                 destination_id = str(data.get_id_by_airport_name(airports, destination))
-                result = "123"
+                print(source_id, destination_id)
                 score = graph.find_best_connection(given_graph=graphik, node1=source_id, node2=destination_id,
-                                                   airlines=airlines,
-                                                   airports=airports, routes=routes)
+                                                   airlines=airlines, airports=airports, routes=routes)
                 print(score)
-
-                # answer.insert(INSERT, result)
-                # answer.pack()
                 my_label.config(text=score)
-
-
         else:
             tk.messagebox.showwarning(title="Error", message="Source and destination airports are required!")
 
@@ -73,42 +66,41 @@ if __name__ == "__main__":
     window = tk.Tk()
     window.title("Flying App")
 
-    frame = tk.Frame(window)
+    frame = tk.Frame(window, bg="#ADD8E6")
     frame.pack()
 
     # Flight parameters frame
-    flight_params_frame = tk.LabelFrame(frame, text="Flight parameters")
+    flight_params_frame = tk.LabelFrame(frame, text="Flight parameters:", bg="#ADD8E6")
     flight_params_frame.grid(row=0, column=0, padx=20, pady=10)
 
-    source_airport_label = tk.Label(flight_params_frame, text="Source airport:")
+    source_airport_label = tk.Label(flight_params_frame, text="Source airport", bg="#FDFD96")
     source_combobox = ttk.Combobox(flight_params_frame, width=40, values=list_of_airports_names())
     source_airport_label.grid(row=0, column=0)
     source_combobox.grid(row=1, column=0)
     source_combobox.bind('<KeyPress>', search_source)
 
-    destination_airport_label = tk.Label(flight_params_frame, text="Destination airport:")
+    destination_airport_label = tk.Label(flight_params_frame, text="Destination airport", bg="#FDFD96")
     destination_combobox = ttk.Combobox(flight_params_frame, width=40, values=list_of_airports_names())
     destination_airport_label.grid(row=0, column=1)
     destination_combobox.grid(row=1, column=1)
     destination_combobox.bind('<KeyPress>', search_destination)
 
-    confirm_button = tk.Button(flight_params_frame, text="Find my flights", command=find_result)
+    confirm_button = tk.Button(flight_params_frame, text="Find my flights", command=find_result, bg="#FDFD96")
     confirm_button.grid(row=1, column=2, sticky="news", padx=20, pady=10)
 
     # Improve padding
     for widget in flight_params_frame.winfo_children():
-        widget.grid_configure(padx=10, pady=5)
+        widget.grid_configure(padx=20, pady=5)
 
     # Search results
-    results_frame = tk.LabelFrame(frame, text="Search results:")
-    results_frame.grid(row=2, column=0, sticky="news", padx=20, pady=10)
+    results_frame = tk.LabelFrame(frame, text="Search results:", bg="#ADD8E6")
+    results_frame.grid(row=2, column=0, sticky="news", padx=20, pady=20)
 
-    terms_check = tk.Label(results_frame)
-    terms_check.grid(row=0, column=0, padx=10, pady=10)
+    result_label = tk.Label(results_frame, bg="#ADD8E6")
+    result_label.grid(row=0, column=0, padx=10, pady=10)
 
     # Create a Label widget
-    my_label = Label(terms_check, text="")
+    my_label = Label(result_label, text="", bg="#ADD8E6")
     my_label.pack()
-    # answer = Text(terms_check, width=50, height=1)
 
     window.mainloop()
